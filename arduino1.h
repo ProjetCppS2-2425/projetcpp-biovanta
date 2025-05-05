270475671d52b1af0d5cadd6fc40eee23e4edd87
nesrine15 <nesrine.allouche@esprit.tn>
2025-05-05 03:14:48 -1100

    implemating arduino

--- arduino1.h
+++ arduino1.h
@@ -5,22 +5,22 @@
 #include <QDebug>
 class arduino
 {
-public:     //méthodes de la classe Arduino
+public:
     arduino();
-    int connect_arduino(); // permet de connecter le PC à Arduino
-    int close_arduino(); // permet de femer la connexion
-    int write_to_arduino( QByteArray ); // envoyer des données vers arduino
-    QByteArray read_from_arduino();  //recevoir des données de la carte Arduino
-    QSerialPort* getserial();   // accesseur
+    int connect_arduino();
+    int close_arduino();
+    int write_to_arduino( QByteArray );
+    QByteArray read_from_arduino();
+    QSerialPort* getserial();
     QString getarduino_port_name();
 private:
-    QSerialPort * serial; //Cet objet rassemble des informations (vitesse, bits de données, etc.)
-    //et des fonctions (envoi, lecture de réception,…) sur ce qu’est une voie série pour Arduino.
+    QSerialPort * serial;
+
     static const quint16 arduino_uno_vendor_id = 0x1A86;
     static const quint16 arduino_uno_producy_id = 0x7523;
     QString arduino_port_name;
     bool arduino_is_available;
-    QByteArray data;  // contenant les données lues à partir d'Arduino
+    QByteArray data;
 };
 
 #endif // ARDUINO1_H
